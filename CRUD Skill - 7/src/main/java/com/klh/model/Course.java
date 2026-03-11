afa835aa3fc43b57d8611b3129e58f1aa24259af
Raghu Nandan Bheeema <raghunandanbheema@gmail.com>
2026-03-11 12:07:01 +0530

    Add files via upload

---
+++ CRUD Skill - 7/src/main/java/com/klh/model/Course.java
@@ -0,0 +1,59 @@
+package com.klh.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    private int courseId;
+    private String title;
+    private String duration;
+    private double fee;
+
+    // Default Constructor
+    public Course() {
+    }
+
+    // Parameterized Constructor
+    public Course(int courseId, String title, String duration, double fee) {
+        this.courseId = courseId;
+        this.title = title;
+        this.duration = duration;
+        this.fee = fee;
+    }
+
+    // Getters and Setters
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+}

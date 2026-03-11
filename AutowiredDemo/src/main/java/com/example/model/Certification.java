6227b390c7d9c71461fa09f1b994f789b7d9f263
Raghu Nandan Bheeema <raghunandanbheema@gmail.com>
2026-03-11 12:08:10 +0530

    Add files via upload

---
+++ AutowiredDemo/src/main/java/com/example/model/Certification.java
@@ -0,0 +1,25 @@
+package com.example.model;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Certification {
+
+    private int id;
+    private String name;
+    private String dateOfCompletion;
+
+    public Certification() {
+        this.id = 101;
+        this.name = "Java Full Stack";
+        this.dateOfCompletion = "2024-05-10";
+    }
+
+    @Override
+    public String toString() {
+        return "Certification [id=" + id +
+               ", name=" + name +
+               ", dateOfCompletion=" + dateOfCompletion + "]";
+    }
+}
---
+++ AutowiredDemo/src/main/java/com/example/model/Student.java
@@ -0,0 +1,29 @@
+package com.example.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+    private int id;
+    private String name;
+    private String gender;
+
+    @Autowired
+    private Certification certification;
+
+    public Student() {
+        this.id = 1;
+        this.name = "Nirmala";
+        this.gender = "Female";
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id +
+               ", name=" + name +
+               ", gender=" + gender +
+               ", certification=" + certification + "]";
+    }
+}
